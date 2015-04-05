9e0c48dbe565149af2e3c3d6ceb4397ef78e4ad6
type0 <ghyghygg30@gmail.com>
2015-04-05 18:56:55 +0800

    radiance map done(?

--- hw1/hw1.m
+++ hw1/hw1.m
@@ -13,17 +13,27 @@ end
 while feof(f)==0
 	lines = strsplit(fgetl(f));
 	file_lists{end+1} = lines{1};
-	B = [B 1 / str2num(lines{2})];
+	B = [B log(str2num(lines{2}))];
 end
 fclose(f);
+imgs={}; %red color for test
+for i = 1:size(file_lists,2)
+	img = imread(file_lists{i});
+	imgs{end+1}=img;
+end
+sample_num = 1000;
+sample_w = randi([1 size(img,1)],sample_num,1);
+sample_h = randi([1 size(img,2)],sample_num,1);
+Z = zeros(sample_num,size(file_lists,2));
+g={};
+lE={};
+for color = 1:3
+	for i = 1:size(file_lists,2)
+		for j = 1:sample_num
+			Z(j,i) = imgs{i}(sample_w(j),sample_h(j),color);
+		end
+	end
 
-%hdr = makehdr(file_lists,'RelativeExposure', B ./ B(1));
+	[g{end+1},lE{end+1}]=gsolve(Z,B,l,w);
 
-% Z={}; %red color for test
-% for i = 1:size(file_lists,2)
-% 	img = imread(file_lists{i});
-% 	Z{end+1}=img;
-% end
-
-%[g,lE]=gsolve(Z,B,l,w);
-
+end
